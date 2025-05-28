140212c8a013d96b6d0daa9748b487d15871f696
Marrimarrink <hihiop@mail.ru>
2025-05-28 20:20:23 +0300

    Initial commit

---
+++ src/main/java/ru/itgirl/storeplatformapplicationchillteam/StorePlatformApplicationChillteamApplication.java
@@ -0,0 +1,13 @@
+package ru.itgirl.storeplatformapplicationchillteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorePlatformApplicationChillteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorePlatformApplicationChillteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itgirl/storeplatformapplicationchillteam/StorePlatformApplicationChillteamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itgirl.storeplatformapplicationchillteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorePlatformApplicationChillteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
