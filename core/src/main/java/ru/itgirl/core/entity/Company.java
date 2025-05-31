4375e0e68ab2419de1f5d1bdbd000dc9133d7afc
TokarevaOlga <oe.tokareva@gmail.com>
2025-05-31 23:26:28 +0300

    feat: 31-05-2025 23:25 Task_16_Core_Create_classes_for_Company

--- core/src/main/java/ru/itgirl/core/entity/Company.java
+++ core/src/main/java/ru/itgirl/core/entity/Company.java
@@ -1,4 +1,21 @@
 package ru.itgirl.core.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name_company", length = 50, nullable = false)
+    private String name_company;
 }
