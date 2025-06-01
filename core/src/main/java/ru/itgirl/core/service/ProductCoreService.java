907c33aa87f0b79835382f0dd7078c4e3890db08
Ekaterina Nevrova <ek.nevrova@mail.ru>
2025-06-01 19:23:52 +0300

    Created class and method CreateProduct

--- core/src/main/java/ru/itgirl/core/service/ProductCoreService.java
+++ core/src/main/java/ru/itgirl/core/service/ProductCoreService.java
@@ -1,9 +1,12 @@
 package ru.itgirl.core.service;
 
+import ru.itgirl.core.dto.ProductCreateDto;
 import ru.itgirl.core.dto.ProductDto;
 
 public interface ProductCoreService {
     ProductDto getProductByName(String name);
 
     ProductDto getProductById(Long id);
+
+    ProductDto createProduct(ProductCreateDto productCreateDto);
 }
