d84e3ac72a33d7eca68c8cd84d2186d1bd62474d
Ekaterina Nevrova <ek.nevrova@mail.ru>
2025-06-01 18:39:37 +0300

    Created basic classes for Product

--- core/src/main/java/ru/itgirl/core/dto/ProductDto.java
+++ core/src/main/java/ru/itgirl/core/dto/ProductDto.java
@@ -1,4 +1,16 @@
 package ru.itgirl.core.dto;
 
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class ProductDto {
+    private Long id;
+    private String name;
+    private List<CompanyDto> companies;
 }
+
