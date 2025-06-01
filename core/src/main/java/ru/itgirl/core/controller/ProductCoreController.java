ab2c90b17c84098d38ad25318108a2452e5d93a8
Ekaterina Nevrova <ek.nevrova@mail.ru>
2025-06-01 19:01:49 +0300

    Created method getProductById

--- core/src/main/java/ru/itgirl/core/controller/ProductCoreController.java
+++ core/src/main/java/ru/itgirl/core/controller/ProductCoreController.java
@@ -1,4 +1,19 @@
 package ru.itgirl.core.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itgirl.core.dto.ProductDto;
+import ru.itgirl.core.service.ProductCoreService;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductCoreController {
+    private final ProductCoreService productCoreService;
+
+    @GetMapping("/product/{id}")
+    public ProductDto getProductById(@PathVariable Long id) {
+        return productCoreService.getProductById(id);
+    }
 }
