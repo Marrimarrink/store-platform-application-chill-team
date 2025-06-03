03a91c61ade3a43a9b3dfd67d8d950396c60ae1a
TokarevaOlga <oe.tokareva@gmail.com>
2025-06-03 14:15:57 +0300

    feat: 03-06-2025 14:15 Task_25_Create_method_addCompany_in_core

--- core/src/main/java/ru/itgirl/core/controller/CompanyCoreController.java
+++ core/src/main/java/ru/itgirl/core/controller/CompanyCoreController.java
@@ -1,10 +1,20 @@
 package ru.itgirl.core.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itgirl.core.dto.CompanyCreateDto;
+import ru.itgirl.core.dto.CompanyDto;
 import ru.itgirl.core.service.CompanyCoreService;
 
+@RestController
 @RequiredArgsConstructor
 public class CompanyCoreController {
     private final CompanyCoreService companyCoreService;
-
+    @PostMapping("/api/v1/company")
+    CompanyDto addCompany(@RequestBody @Valid CompanyCreateDto companyCreateDto) {
+        return companyCoreService.addCompany(companyCreateDto);
+    }
 }
