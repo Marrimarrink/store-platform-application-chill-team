b28702da3772305459022a629ef647de4273dceb
TokarevaOlga <oe.tokareva@gmail.com>
2025-06-03 17:26:27 +0300

    fix: 03-06-2025 17:30 Task_25_Fix_mapping_for_method_addCompany_in_core

--- core/src/main/java/ru/itgirl/core/controller/CompanyCoreController.java
+++ core/src/main/java/ru/itgirl/core/controller/CompanyCoreController.java
@@ -4,16 +4,19 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.itgirl.core.dto.CompanyCreateDto;
 import ru.itgirl.core.dto.CompanyDto;
 import ru.itgirl.core.service.CompanyCoreService;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class CompanyCoreController {
     private final CompanyCoreService companyCoreService;
-    @PostMapping("/api/v1/company")
+
+    @PostMapping("/v1/companies")
     CompanyDto addCompany(@RequestBody @Valid CompanyCreateDto companyCreateDto) {
         return companyCoreService.addCompany(companyCreateDto);
     }
