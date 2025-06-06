10665eac0ab5fd30dd1f4c3ebc5d9dedb9f79c86
Ekaterina Nevrova <ek.nevrova@mail.ru>
2025-06-06 19:08:06 +0300

    Fixed @Valid in Product's methods

--- web/src/main/java/ru/itgirl/web/feign/ProductCoreServiceClient.java
+++ web/src/main/java/ru/itgirl/web/feign/ProductCoreServiceClient.java
@@ -1,5 +1,6 @@
 package ru.itgirl.web.feign;
 
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import ru.itgirl.core.dto.ProductCreateDto;
@@ -10,11 +11,11 @@ import java.util.List;
 
 @FeignClient(
         name = "ProductCore",
-        url = "http://localhost:8082"
+        url = "http://localhost:8080"
 )
 public interface ProductCoreServiceClient {
     @PostMapping("/api/v1/products")
-    ProductDto addProduct(@RequestBody /*@Valid*/ ProductCreateDto productCreateDto);
+    ProductDto addProduct(@RequestBody @Valid ProductCreateDto productCreateDto);
 
     @GetMapping("/api/v1/products/{id}")
     ProductDto getProductById(@PathVariable("id") Long id);
@@ -23,7 +24,7 @@ public interface ProductCoreServiceClient {
     List<ProductDto> getAllProducts();
 
     @PutMapping("/api/v1/products/{id}")
-    ProductDto updateProduct(@PathVariable("id") Long id, @RequestBody /*@Valid*/ ProductUpdateDto productUpdateDto);
+    ProductDto updateProduct(@PathVariable("id") Long id, @RequestBody @Valid ProductUpdateDto productUpdateDto);
 
     @DeleteMapping("/api/v1/products/{id}")
     void deleteProduct(@PathVariable("id") Long id);
